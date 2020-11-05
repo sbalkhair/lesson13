2b41031c6f9cce06b43707243e6b00990af2cec1
saeed <s.balkhair@hotmail.com>
2020-11-05 09:51:20 +0300

    Initial check-in of module lesson13

---
+++ Person.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Security.Policy;
+
+namespace lesson13
+{
+    public class Person
+    {
+        //private string _name;
+        //private int _age;
+
+        //public void setName(string name)
+        //{
+        //    _name = name;
+        //}
+
+        //public string getName()
+        //{
+        //    return _name;
+        //}
+
+        private int _age;
+        public string Name { get; set; }
+        public bool Ismmaarid { get; set; }
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                if (value > 0)
+                {
+                    _age = value;
+                }
+                else
+                {
+                    _age = (-1) * value;
+                }
+            }
+        }
+
+
+        //public void setAge(int age)
+        //{
+        //    if (age > 0)
+        //    {
+        //        _age = age;
+        //    }
+        //    else
+        //    {
+        //        _age = age * (-1);
+        //    }
+
+        //}
+        //public int getAge()
+        //{
+        //    return _age;
+        //}
+
+        //public int add(int x, int y)
+        //{
+        //    return x + y;
+        //}
+        //public int add(int x, int y, int z)
+        //{
+        //    return x + y + z;
+        //}
+
+        //public int add(params int[] numbers)
+        //{
+        //    var sum = 0;
+        //    foreach (var item in numbers)
+        //    {
+        //        sum += item;
+        //    }
+        //    return sum;
+        //}
+
+        //public void caculate(int x, int y, out int sum, out int sub)
+        //{
+        //    sum = x + y;
+        //    sub = x - y;
+        //}
+
+        //public int calculateAge(ref int age)
+        //{
+        //    age = age + 5;
+        //    return age;
+        //}
+
+
+        //public void GetInfo()
+        //{
+        //    Console.WriteLine($"name is {Name} and your age : {Age}");
+        //}
+
+        public decimal decimalGetSalary2020()
+        {
+            return GetSalary() - 10000;
+        }
+
+        private decimal GetSalary()
+        {
+            return 50000;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace lesson13
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //var obj1 = new Person { Name = "roay", Age = 31 };
+            //var obj2 = new Person { Name = "saeed", Age = 300 };
+
+            var obj1 = new Person();
+            //Console.WriteLine(obj1.add(new int[] { 2, 3 }));
+            //Console.WriteLine(obj1.add(2, 3));
+            //int x;
+            //int y;
+            //obj1.caculate(1, 2, out x, out y);
+            //Console.WriteLine($"sum:{x}");
+            //Console.WriteLine($"sub:{y}");
+
+
+            //var age = 10;
+
+            //obj1.calculateAge(ref age);
+            //Console.WriteLine(age);
+
+            Console.WriteLine(obj1.decimalGetSalary2020());
+            Console.ReadKey();
+        }
+    }
+
+
+}
